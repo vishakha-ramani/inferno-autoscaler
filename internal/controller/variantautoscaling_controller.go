fa46a69c701e5c30e2dd218060edf4533cf34a4a
Tommaso Sgreccia <tommaso.sgreccia01@gmail.com>
2025-09-28 15:38:26 -0400

    changed scale to zero logging - disabled by default

--- internal/controller/variantautoscaling_controller.go
+++ internal/controller/variantautoscaling_controller.go
@@ -99,7 +99,7 @@ func (r *VariantAutoscalingReconciler) Reconcile(ctx context.Context, req ctrl.R
 		}
 	}
 
-	if os.Getenv("WVA_SCALE_TO_ZERO") != "false" {
+	if os.Getenv("WVA_SCALE_TO_ZERO") != "false" && os.Getenv("WVA_SCALE_TO_ZERO") != "" {
 		logger.Log.Info("Scaling to zero is enabled!")
 	}
 

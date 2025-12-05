80356415959300f201184ee07f5d11aeecb73226
Mohammed Abdi <mohammed.munir.abdi@ibm.com>
2025-12-05 12:03:13 -0500

    address review
    Signed-off-by: Mohammed Abdi <mohammed.munir.abdi@ibm.com>

--- internal/controller/predicates.go
+++ internal/controller/predicates.go
@@ -1,19 +1,3 @@
-/*
-Copyright 2025.
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
-
 package controller
 
 import (
@@ -22,16 +6,23 @@ import (
 	"sigs.k8s.io/controller-runtime/pkg/predicate"
 )
 
-// ConfigMapPredicate returns a predicate that filters ConfigMap events to only the target ConfigMap.
-// It checks that the ConfigMap name matches configMapName and namespace matches configMapNamespace.
+// ConfigMapPredicate returns a predicate that filters ConfigMap events to only the target ConfigMaps.
+// It matches the enqueue function logic:
+// - Allows configMapName from any namespace
+// - Allows saturationConfigMapName only if namespace matches configMapNamespace
+// This predicate is used to filter only the target configmap.
 func ConfigMapPredicate() predicate.Predicate {
 	return predicate.NewPredicateFuncs(func(obj client.Object) bool {
-		return obj.GetName() == configMapName && obj.GetNamespace() == configMapNamespace
+		name := obj.GetName()
+		return name == configMapName || (name == saturationConfigMapName && obj.GetNamespace() == configMapNamespace)
 	})
 }
 
 // ServiceMonitorPredicate returns a predicate that filters ServiceMonitor events to only the target ServiceMonitor.
 // It checks that the ServiceMonitor name matches serviceMonitorName and namespace matches configMapNamespace.
+// This predicate is used to filter only the target ServiceMonitor.
+// The ServiceMonitor is watched to enable detection when it is deleted, which would prevent
+// Prometheus from scraping controller metrics (including optimized replicas).
 func ServiceMonitorPredicate() predicate.Predicate {
 	return predicate.NewPredicateFuncs(func(obj client.Object) bool {
 		return obj.GetName() == serviceMonitorName && obj.GetNamespace() == configMapNamespace
